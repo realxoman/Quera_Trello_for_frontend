eaac16bf385da8bedad1baaa1b287434498404d8
Ali Esmaeili <fromiran98@gmail.com>
2023-09-12 11:17:40 +0330

    Fix: Settings

--- settings/api/router.py
+++ settings/api/router.py
@@ -1,12 +1,12 @@
 from rest_framework import routers
-from django.urls import path
+from django.urls import path, include
 from settings.api.viewset import SettingViewSet
 
 
 app_name = 'setting_router'
 router = routers.DefaultRouter()
+router.register('settings', SettingViewSet, basename='settings')
 
 urlpatterns = [
-    path('settings/',
-         SettingViewSet.as_view(), name='settings'),
+    path('', include(router.urls))
 ]
