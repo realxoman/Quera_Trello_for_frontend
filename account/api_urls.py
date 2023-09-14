0b787957cd2d991714d60b6dc03fb405262d2be3
Ali Esmaeili <fromiran98@gmail.com>
2023-09-14 15:31:56 +0330

    fix: router problem

--- account/api_urls.py
+++ account/api_urls.py
@@ -3,11 +3,15 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from account.api.viewset import CustomTokenObtainPairViewSet
 
+from account.api.router import router
+
 app_name = 'account_urls'
 urlpatterns = [
     path('account/', include('account.api.router')),
+    
     path('account/token/',
          CustomTokenObtainPairViewSet.as_view(), name='token-login'),
     path('account/token/refresh/',
          TokenRefreshView.as_view(), name='token-refresh'),
+    path('account/', include(router.urls)),
 ]
