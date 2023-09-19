f71a0cddcef020e6b90f65f40a422524092e8b61
Ali Esmaeili <fromiran98@gmail.com>
2023-09-19 15:08:21 +0330

    Fix: Add thumbnail into api accounts

--- account/api/serializers/serializer_user_register.py
+++ account/api/serializers/serializer_user_register.py
@@ -19,7 +19,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'password',
-                  'first_name', 'last_name', 'phone_number']
+                  'first_name', 'last_name', 'phone_number', 'thumbnail']
 
     def validate(self, value):
         if get_user_model().objects.filter(username=value).exists():
